b2bf5f5bd7d3886d6b0fee33e7c42ec961e78805
Casey Jones <jonescaseyb@gmail.com>
2024-09-20 18:38:57 -0500

    use accordion for sub results

--- src/sub-results.tsx
+++ src/sub-results.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Box, Text } from 'ink';
 import { ObjectId } from 'mongodb';
+import { Accordion } from './accordion.js';
 
-const toDisplay = (v: any) => {
+const toDisplay = (k: string, v: any) => {
 	if (v instanceof ObjectId) {
-		return <Text>{v.toString()}</Text>;
+		return (
+			<>
+				<Text>{k}: </Text>
+				<Text>{v.toString()}</Text>
+			</>
+		);
 	} else if (typeof v !== 'object') {
-		return <Text>{v}</Text>;
+		return (
+			<>
+				<Text>{k}: </Text>
+				<Text>{v}</Text>
+			</>
+		);
 	} else {
-		return <SubResults result={v} />;
+		return (
+			<Accordion header={k}>
+				<>
+					<SubResults result={v} />
+				</>
+			</Accordion>
+		);
 	}
 };
+
 export const SubResults = (props: { result: object }) => {
 	return (
 		<Box flexDirection="column">
 			{Object.entries(props.result).map(([k, v]) => (
 				<Box key={k} flexDirection="row">
-					<Text>{k}: </Text>
-					{toDisplay(v)}
+					{toDisplay(k, v)}
 				</Box>
 			))}
 		</Box>
